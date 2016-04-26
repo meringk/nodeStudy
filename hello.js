dfd50fdd730d9dd0a251a7bbbec701af0c5b518f
meringk <mering_k@naver.com>
2016-04-26 16:31:37 +0900

    express

--- hello.js
+++ hello.js
@@ -1,14 +1,18 @@
-var http = require('http');
-var express = require('express');
+var express=require('express');
+var fs=require('fs');
 var app=express();
 
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello world!');
-}).listen(3000);
+app.listen(3000,function(){
+	console.log('서버 시작됨 3000 포트');
+});
 
-//app.get('/',function(req,res){
-//	res.sendFile(__dirname+'index.html');
-//})
-console.log('서버 시작됨 3000 포트');
-
+app.get('/',function(req,res){
+	fs.readFile('index.html',function(error,data){
+		if(error){
+			console.log(error);
+		}else{
+			res.writeHead(200,{'Content-Type':'text/html'});
+			res.end(data);
+		}
+	});
+});
