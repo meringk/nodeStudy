b69fdcb384c2271a733308e1b90a77885bd259cb
meringk <mering_k@naver.com>
2016-04-26 17:11:02 +0900

    Added files via upload

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ var fs=require('fs');
 var path=require('path');
 var app=express();
 
-app.use(express.static(path.join(__dirname+'public')));
+app.use(express.static(path.join(__dirname+'/public')));
 
 app.listen(3000,function(){
 	console.log('서버 시작됨 3000 포트');
