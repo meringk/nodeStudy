edd9404be37fcbf529c35e95cc961a2a537b3647
meringk <mering_k@naver.com>
2016-04-26 16:58:06 +0900

    Added files via upload

--- hello.js
+++ hello.js
@@ -1,11 +1,15 @@
 var express=require('express');
 var fs=require('fs');
+var path=require('path');
 var app=express();
+
+app.use(express.static(path.join(__dirname+'nodeStudy')));
 
 app.listen(3000,function(){
 	console.log('서버 시작됨 3000 포트');
 });
 
+/*
 app.get('/',function(req,res){
 	fs.readFile('index.html',function(error,data){
 		if(error){
@@ -15,12 +19,4 @@ app.get('/',function(req,res){
 			res.end(data);
 		}
 	});
-	fs.readFile('index.css',function(error,data){
-		if(error){
-			console.log(error);
-		}else{
-			res.writeHead(200,{'Content-Type':'text/html'});
-			res.end(data);
-		}
-	});
-});
+});*/
