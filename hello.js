583ac61f2f1ffac36db48d5eaf77b2d32bf13fa6
meringk <mering_k@naver.com>
2016-04-26 16:18:58 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var express = require('express');
+var app=express();
 
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello world!');
+	//res.end('Hello world!');
 }).listen(3000);
 
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'index.html');
+})
 console.log('서버 시작됨 3000 포트');
+
